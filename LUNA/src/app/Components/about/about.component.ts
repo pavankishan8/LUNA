1e18b53dfa7f2a566e741145c1106df9625b8280
pavan.rajaiah <pavan.rajaiah@starmarkit.com>
2023-11-20 11:08:38 +0530

    Update.

--- LUNA/src/app/Components/about/about.component.ts
+++ LUNA/src/app/Components/about/about.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  typedValue: string = '';
+  values: string[] = [];
 
+  addValue() {
+    if (this.typedValue.trim() !== '') {
+      this.values.push(this.typedValue);
+      this.typedValue = '';
+    }
+  }
+
+  paragraph = 'Shah Rukh Khan';
+
+  userInput = '';
+  startTime: Date;
+  endTime: Date;
+  elapsedTime: number;
+
+  onInputChange() {
+    if (this.userInput === this.paragraph) {
+      this.endTime = new Date();
+      this.calculateElapsedTime();
+    }
+  }
+
+  calculateElapsedTime() {
+    const startMilliseconds = this.startTime.getTime();
+    const endMilliseconds = this.endTime.getTime();
+    this.elapsedTime = (endMilliseconds - startMilliseconds) / 1000;
+  }
+
+  startTimer() {
+    this.startTime = new Date();
+  }
+  
 }
